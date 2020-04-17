22c9fc4cd03e3b1c207a5acb026d3bf3bef43645
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2020-04-17 18:51:31 +0100

    Check for required env vars

--- test-video.sh
+++ test-video.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ -z "${MUX_TOKEN_ID:-}" ]
+then
+      echo "MUX_TOKEN_ID not set"
+      exit 255
+fi
+
+if [ -z "${MUX_TOKEN_SECRET:-}" ]
+then
+      echo "MUX_TOKEN_SECRET not set"
+      exit 255
+fi
+
 TESTS=./examples/video/exercise*
 for f in $TESTS
 do
